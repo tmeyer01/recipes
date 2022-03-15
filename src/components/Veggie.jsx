2504036214fa8be5b59d219f72aa908574e68a6c
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-15 14:56:58 -0700

    created vegan recipe component

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
+import {
+  Wrapper,
+  Card,
+  Gradient
+} from './veggieElements'
+
 
 function Veggie() {
+
+  const [veggie, setVeggie] = useState([]);
+
+  useEffect(() => {
+    getVeggie();
+  }, []);
+
+  const getVeggie = async () => {
+    
+    const check = localStorage.getItem('veggie');
+
+    if(check){
+      setVeggie(JSON.parse(check))
+    }else{
+      const api = await fetch(
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10&tags=vegan`
+      );
+      const data = await api.json();
+      localStorage.setItem('veggie', JSON.stringify(data.recipes))
+      
+      setVeggie(data.recipes);
+      console.log(data)
+    }
+    
+   
+  };
   return (
-    <div>Veggie</div>
+    <div>
+      
+    <Wrapper>
+      <h3>Vegan Picks</h3>
+      <Splide options={{
+        perPage: 3,
+        arrows: false,
+        pagination: false,
+        drag: 'free',
+        gap: "5rem",
+      }}>
+        {veggie.map((recipe) => {
+          return (
+            <SplideSlide key={recipe.id}>
+              <Card>
+                <p>{recipe.title}</p>
+                <img src={recipe.image} alt={recipe.title} />
+                <Gradient />
+              </Card>
+            </SplideSlide>
+          );
+        })}
+      </Splide>
+    </Wrapper>
+   
+  </div>
   )
 }
 
