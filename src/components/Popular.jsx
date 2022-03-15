4972a87e89971714d7b8ac3cf7fef78bb66ed987
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-15 12:38:42 -0700

    Use splide to create carrousel of images

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,34 +1,60 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/splide.min.css";
 
 function Popular() {
-  
+  const [popular, setPopular] = useState([]);
 
-  const [popular, setPopular ] = useState ([]);
-
-  useEffect(()=> {
+  useEffect(() => {
     getPopular();
-  },[])
+  }, []);
 
-   const getPopular = async () => {
-     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`);
-     const data = await api.json();
-     setPopular(data.recipes);
-     
-   }
-  
-  
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
+    );
+    const data = await api.json();
+    setPopular(data.recipes);
+    //  console.log(data)
+  };
+
   return (
     <div>
-      {popular.map((recipe) => {
-        return(
-         <div key={recipe.id}> 
-           <p>{recipe.title}</p>
-         </div> 
-      
-        )
-      })}
+     
+        return (
+          <Wrapper>
+            <h3>Popular Picks</h3>
+            <Splide>
+            {popular.map((recipe) => {
+                return (
+                  <SplideSlide>
+                    <Card>
+                      <p key={recipe.id}>{recipe.title}</p>
+                      <img src={recipe.image} alt={recipe.title} />
+                    </Card>
+                  </SplideSlide>
+                );
+              })}
+            </Splide>
+          </Wrapper>
+        );
+    
     </div>
-  )
+  );
 }
 
-export default Popular
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img {
+    border-radius: 2rem;
+  }
+`;
+export default Popular;
