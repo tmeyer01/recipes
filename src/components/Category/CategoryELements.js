fec7402686d5e5bb50de52419d3b05e01fef5c30
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-16 14:44:38 -0700

    Created Category component dir and seperated syled components

---
+++ src/components/Category/CategoryELements.js
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import {NavLink} from 'react-router-dom'
+
+export const List = styled.div`
+  display:flex;
+  justify-content: center;
+  margin: 2rem 0rem;
+`;
+
+export const SLink = styled(NavLink)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-radius: 50%;
+  margin-right: 2rem;
+  text-decoration: none;
+  background: linear-gradient( 35deg, #494949, #313131);
+  width:6rem;
+  height: 6rem;
+  cursor: pointer;
+  transform: scale(0.8);
+
+  h4{
+    color: #fff;
+    font-size:0.8rem
+  }
+  svg{
+    color: #fff;
+    font-size: 1.5rem
+  }
+
+  &.active {
+    background: linear-gradient(to right, #f27121, #e94057);
+
+    h4{
+      color: #fff;
+    }
+    svg{
+      color: #fff;
+    }
+  }
+
+`
--- src/components/Category/index.jsx
+++ src/components/Category/index.jsx
@@ -1,36 +1,31 @@
 import {FaPizzaSlice, FaHamburger} from 'react-icons/fa';
 import {GiNoodles, GiChopsticks} from 'react-icons/gi';
-import styled from 'styled-components';
+import { List, SLink } from './CategoryELements';
 
 import {NavLink} from 'react-router-dom'
 
 function Category() {
   return (
     <List>
-      <NavLink to={'/cuisine/Italian'}>
+      <SLink  to={'/cuisine/Italian'}>
         <FaPizzaSlice/>
         <h4>Italian</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/American'}>
+      </SLink>
+      <SLink  to={'/cuisine/American'}>
         <FaHamburger/>
         <h4>American</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Thai'}>
+      </SLink>
+      <SLink  to={'/cuisine/Thai'}>
         <GiNoodles/>
         <h4>Thai</h4>
-      </NavLink>
-      <NavLink to={'/cuisine/Japanese'}>
+      </SLink>
+      <SLink to={'/cuisine/Japanese'}>
         <GiChopsticks/>
         <h4>Japanese</h4>
-      </NavLink>
+      </SLink>
     </List>
   )
 }
 
-const List = styled.div`
-  display:flex;
-  justify-content: center;
-  margin: 2rem 0rem;
-`;
 
 export default Category
