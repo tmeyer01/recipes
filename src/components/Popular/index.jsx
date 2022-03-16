ea87cea2c8b21724c0bb4ca56e7a253efd418c7a
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-16 10:11:08 -0700

    removed import of styled-component

--- src/components/Popular/index.jsx
+++ src/components/Popular/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import styled from "styled-components";
+
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 import {
