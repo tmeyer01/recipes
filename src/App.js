070335a49d3c505f06974e0572275719a7587410
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-16 16:10:09 -0700

    Added search comp to browser routes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Pages from "./pages/Pages";
 import './index.css'
 import Category from "./components/Category/index";
 import {BrowserRouter} from 'react-router-dom'
+import Search from "./components/Search";
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
-      
+      <Search />
       <Category />
       <Pages />
       
