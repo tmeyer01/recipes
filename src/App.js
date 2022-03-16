c5e91b61d0d07ddb1520ff1344b2845e5d0e19b1
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-16 11:35:05 -0700

    Added Category compontent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Pages from "./pages/Pages";
 import './index.css'
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
+      <Category />
       <Pages />
     </div>
   );
