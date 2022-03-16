d32376dc0a3f6408f9470fb5e6a0442b7f895e58
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-16 11:42:38 -0700

    moved browser-route up a level

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Pages from "./pages/Pages";
 import './index.css'
 import Category from "./components/Category";
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div className='App'>
+      <BrowserRouter>
+      
       <Category />
       <Pages />
+      
+      </BrowserRouter>
     </div>
   );
 }
