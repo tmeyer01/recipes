560f612c94a5421bb7d0194f0130bed3b975930e
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-17 11:44:33 -0700

    Cards now Links

--- src/components/Popular/index.jsx
+++ src/components/Popular/index.jsx
@@ -1,13 +1,8 @@
 import { useEffect, useState } from "react";
-
+import { Link } from "react-router-dom";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import {
-  Wrapper,
-  Card,
-  Gradient
-} from './PopularElements'
-
+import { Wrapper, Card, Gradient } from "./PopularElements";
 
 function Popular() {
   const [popular, setPopular] = useState([]);
@@ -17,55 +12,52 @@ function Popular() {
   }, []);
 
   const getPopular = async () => {
-    
-    const checkLocalStorgae = localStorage.getItem('popular');
+    const checkLocalStorgae = localStorage.getItem("popular");
 
-    if(checkLocalStorgae){
-      setPopular(JSON.parse(checkLocalStorgae))
-    }else{
+    if (checkLocalStorgae) {
+      setPopular(JSON.parse(checkLocalStorgae));
+    } else {
       const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10`
+        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10&tags=vegan`
       );
       const data = await api.json();
-      localStorage.setItem('popular', JSON.stringify(data.recipes))
-      
+      localStorage.setItem("popular", JSON.stringify(data.recipes));
+
       setPopular(data.recipes);
-      console.log(data)
+      console.log(data);
     }
-    
-   
   };
 
   return (
     <div>
-      
       <Wrapper>
         <h3>Popular Picks</h3>
-        <Splide options={{
-          perPage: 3,
-          arrows: false,
-          pagination: false,
-          drag: 'free',
-          gap: "5rem",
-        }}>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
           {popular.map((recipe) => {
             return (
               <SplideSlide key={recipe.id}>
-                <Card>
-                  <p>{recipe.title}</p>
-                  <img src={recipe.image} alt={recipe.title} />
-                  <Gradient />
-                </Card>
+                <Link to={'/recipe/' + recipe.id}>
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Card>
+                </Link>
               </SplideSlide>
             );
           })}
         </Splide>
       </Wrapper>
-     
     </div>
   );
 }
-
-
 
 export default Popular;
--- src/components/Vegan/index.jsx
+++ src/components/Vegan/index.jsx
@@ -1,15 +1,10 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import {
-  Wrapper,
-  Card,
-  Gradient
-} from './VeganElements'
-
+import { Wrapper, Card, Gradient } from "./VeganElements";
 
 function Veggie() {
-
   const [veggie, setVeggie] = useState([]);
 
   useEffect(() => {
@@ -17,52 +12,51 @@ function Veggie() {
   }, []);
 
   const getVeggie = async () => {
-    
-    const check = localStorage.getItem('veggie');
+    const check = localStorage.getItem("veggie");
 
-    if(check){
-      setVeggie(JSON.parse(check))
-    }else{
+    if (check) {
+      setVeggie(JSON.parse(check));
+    } else {
       const api = await fetch(
         `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10&tags=vegan`
       );
       const data = await api.json();
-      localStorage.setItem('veggie', JSON.stringify(data.recipes))
-      
+      localStorage.setItem("veggie", JSON.stringify(data.recipes));
+
       setVeggie(data.recipes);
-      console.log(data)
+      console.log(data);
     }
-    
-   
   };
   return (
     <div>
-      
-    <Wrapper>
-      <h3>Vegan Picks</h3>
-      <Splide options={{
-        perPage: 3,
-        arrows: false,
-        pagination: false,
-        drag: 'free',
-        gap: "5rem",
-      }}>
-        {veggie.map((recipe) => {
-          return (
-            <SplideSlide key={recipe.id}>
-              <Card>
-                <p>{recipe.title}</p>
-                <img src={recipe.image} alt={recipe.title} />
-                <Gradient />
-              </Card>
-            </SplideSlide>
-          );
-        })}
-      </Splide>
-    </Wrapper>
-   
-  </div>
-  )
+      <Wrapper>
+        <h3>Vegan Picks</h3>
+        <Splide
+          options={{
+            perPage: 3,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {veggie.map((recipe) => {
+            return (
+              <SplideSlide key={recipe.id}>
+                <Link to={"/recipe/" + recipe.id}>
+                  <Card>
+                    <p>{recipe.title}</p>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <Gradient />
+                  </Card>
+                </Link>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
+    </div>
+  );
 }
 
-export default Veggie
+export default Veggie;
