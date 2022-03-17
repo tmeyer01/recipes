c1ca7081cc72d3af6177ade5778c7510134292ee
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-17 11:28:36 -0700

    Moved Search component to new dir and deleted old component

---
+++ src/components/Search/SearchElements.jsx
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const FormStyle = styled.form`
+margin: 0rem 20rem;
+
+
+div{
+  width: 100%;
+  position: relative;
+}
+
+input{
+  border:none;
+  background: linear-gradient(35deg, #494949, #313131);
+  font-size: 1.5rem;
+  color: #fff;
+  padding: 1rem 3rem;
+  border: none;
+  border-radius: 1rem;
+  outline: none;
+  width: 100%;
+}
+svg{
+  position:absolute;
+  top: 50%;
+  left: 0%;
+  transform: translate(100%, -50%);
+  color: #fff;
+}
+
+`
--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,5 +1,5 @@
 import {  useState  } from 'react';
-import styled from 'styled-components';
+import { FormStyle } from './SearchElements';
 import {  FaSearch  } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 
@@ -31,35 +31,7 @@ function Search() {
   )
 }
 
-const FormStyle = styled.form`
-  margin: 0rem 20rem;
- 
- 
-  div{
-    width: 100%;
-    position: relative;
-  }
 
-  input{
-    border:none;
-    background: linear-gradient(35deg, #494949, #313131);
-    font-size: 1.5rem;
-    color: #fff;
-    padding: 1rem 3rem;
-    border: none;
-    border-radius: 1rem;
-    outline: none;
-    width: 100%;
-  }
-  svg{
-    position:absolute;
-    top: 50%;
-    left: 0%;
-    transform: translate(100%, -50%);
-    color: #fff;
-  }
-
-`
 
 
 export default Search
