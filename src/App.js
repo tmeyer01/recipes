811c6542fb215e832eb969c887b77f5bc6292438
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-17 18:42:55 -0700

    Add nav component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './index.css'
 import Category from "./components/Category/index";
 import {BrowserRouter} from 'react-router-dom'
 import Search from "./components/Search";
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
+      <Nav />
       <Search />
       <Category />
       <Pages />
