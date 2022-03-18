2a7d16262c9a6d17bad799ca37b9e4d0779bb2ec
ThomasMeyer <tmeyerwebdev@gmail.com>
2022-03-17 18:20:25 -0700

    cards now link

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from 'react'
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 
@@ -27,8 +27,10 @@ function Searched() {
     {searchedRecipes.map((item) => {
       return(
         <Card key={item.id}>
+          <Link to={'/recipe/' + item.id}>
           <img src={item.image} alt=''/>
           <h4>{item.title}</h4>
+          </Link>
         </Card>
       )
     }) }
